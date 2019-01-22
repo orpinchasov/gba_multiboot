ef7fc727e77575674f00099c049b8ddeaba5f332
Or Pinchasov <or.pinchasov@gmail.com>
2019-01-22 02:59:54 +0200

    improved python xboo implementation

--- python/multiboot.py
+++ python/multiboot.py
@@ -4,6 +4,7 @@
 # The implementation is based on the original
 # implementation in the XBOO cable.
 
+import sys
 from time import sleep
 from struct import pack, unpack
 
@@ -22,14 +23,22 @@ def update_arduino(s, bytes):
 
 def read(s):
     raw = s.read(2)
-    return unpack('<H', raw)[0]
+    try:
+        return unpack('<H', raw)[0]
+    except Exception as e:
+        print('raw:', raw.encode('hex'))
+        return 0xffff
     
     
 def transmission(s, bytes):
-    print 'Writing: %04x' % (bytes,)
     update_arduino(s, bytes)
     result = read(s)
-    print 'Read: %04x' % (result,)
+    if type(bytes) is int:
+        print 'send/receive: %04x %04x' % (bytes, result)
+    else:
+        print 'send/receive: %s %04x' % (bytes.encode('hex'), result)
+        
+    sleep(0.01)
     
     return result
 
@@ -42,7 +51,7 @@ def open_serial():
     return s
 
 
-def initialize(s):
+def initialize(s, data):
     print 'Waiting for first response...'
     
     response = False
@@ -71,33 +80,41 @@ def initialize(s):
             sleep(0.0625)
             return
             
-        sleep(0.0625)
+        #sleep(0.0625)
         
-    
     r = transmission(s, 0x6102)
-    sleep(0.06)
     
     print 'Done\nSending Header Data......'
     
     for i in xrange(0x60):
-        transmission(s, i)
-        sleep(0.0625)
+        transmission(s, data[i * 2] + data[i * 2 + 1])
         
     transmission(s, 0x6202)
-    sleep(0.0625)
     transmission(s, 0x6202)
-    sleep(0.0625)
     
-    for i in xrange(100):
-        transmission(0x63f7)
-        sleep(0.0625)
+    r = transmission(s, 0x63f7)
+    while (r & 0xff00) != 0x7300:
+        r = transmission(s, 0x63f7)
+        
+    # Get the encryption
+    cc = r & 0xff
+    hh = ((cc & 0xff) + 0x20f) & 0xff
+    enc = ((cc & 0xff) << 8) | (0x0ffff00f7)
+
+    transmission(s, 0x6400 | hh)
+
+    sleep(0.06)
 
 
 def main():
     s = open_serial()
+    data = open(sys.argv[1], 'rb').read()
+    
+    padding = (16 - (len(data) % 16)) % 16
+    data = data + '\x00' * padding
     
     while True:
-        initialize(s)
+        initialize(s, data)
 
 
 if __name__ == '__main__':
